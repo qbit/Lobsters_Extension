c113c355d1ee57944a010e3d98c20b68f25eb891
Aaron Bieber <qbit@deftly.net>
2012-08-30 08:26:45 -0600

    added license info and default sentiment

--- ext.js
+++ ext.js
@@ -1,3 +1,30 @@
+// Sentiment code taken from git://github.com/thinkroth/Sentimental.git
+/*
+## Sentimental License
+  
+(The MIT License) 
+  
+Copyright (c) 2012 Kevin M Roth &lt;kevinroth82@gmail.com&gt; 
+  
+Permission is hereby granted, free of charge, to any person obtaining 
+a copy of this software and associated documentation files (the   
+'Software'), to deal in the Software without restriction, including   
+without limitation the rights to use, copy, modify, merge, publish,   
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to 
+the following conditions: 
+  
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.   
+  
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,   
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  
+*/
 console.log( "running" );
 //
 var se = document.createElement( 'script' );
@@ -83,7 +110,9 @@ for ( ci = 0, cl = comments.length; ci < cl; ci++ ) { \
     msg += ':('; \
  } else if ( score.negative.score < score.positive.score ) { \
     msg += ':)'; \
- } \
+ } else { \
+    msg += 'Can not tell if positive or negative'; \
+ }\
  var old = comments[ci].innerHTML; \
  comments[ci].innerHTML = msg + '</b>' + old; \
 }\
